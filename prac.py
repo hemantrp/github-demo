7e9749519fdca74da6c5852a5f31d59dfc54e062
Hemant <hemantrpatil98@gmail.com>
2021-05-28 14:45:52 +0530

    added func headers

--- prac.py
+++ prac.py
@@ -1,11 +1,15 @@
+#Add implementation
 def add(x,y):
     pass
-    
+
+#sub implementation
 def subtract(x,y):
     pass
 
+#mul implementation
 def multiply(x,y):
     pass
     
+#div implementation    
 def divide(x,y):            
     pass
