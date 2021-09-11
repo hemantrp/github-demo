5cbf3ffbf9542b0e7ebfd972a35fac72b0d989ad
Hemant <hemantrpatil98@gmail.com>
2021-09-11 09:50:55 +0530

    Trial

---
+++ prac.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout<<"Hello world";
+    int a;
+    cout<<"\nEnter Number :";
+    cin>>a;
+
+    cout<<"Number is :"<<a;
+    return 0;
+}
